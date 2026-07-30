9fd6b150839140ca5d5fcf3171f390deaada5368
rangpurquantum-web <rangpurquantum@gmail.com>
2026-07-30 15:51:49 +0600

    Fix prisma.config.ts by removing earlyAccess and correcting migrate property to migrations

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,16 +2,8 @@ import path from "node:path";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  export default defineconfig({
   schema: path.join("prisma", "schema.prisma"),
-  migrate: {
-    seed: {
-      run: "ts-node",
-      args: [
-        "--compiler-options",
-        '{"module":"CommonJS"}',
-        "prisma/seed.ts",
-      ],
-    },
+  migrations: {
+    seed: 'ts-node --compiler-options "{\\"module\\":\\"CommonJS\\"}" prisma/seed.ts',
   },
 });
