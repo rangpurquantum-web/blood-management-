3f90247bb3442cf427edfd4068aa38d6d25642dd
rangpurquantum-web <rangpurquantum@gmail.com>
2026-08-10 14:20:15 +0600

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,18 +1,20 @@
 import { DefaultSession } from "next-auth";
 
-type Role = "ADMIN" | "VOLUNTEER";
+type Role = "SUPER_ADMIN" | "ADMIN" | "VOLUNTEER";
 
 declare module "next-auth" {
   interface Session {
     user: {
       role?: Role;
       permissions?: any;
+      branchId?: number | null;
     } & DefaultSession["user"];
   }
 
   interface User {
     role?: Role;
     permissions?: any;
+    branchId?: number | null;
   }
 }
 
@@ -20,5 +22,6 @@ declare module "next-auth/jwt" {
   interface JWT {
     role?: Role;
     permissions?: any;
+    branchId?: number | null;
   }
 }
