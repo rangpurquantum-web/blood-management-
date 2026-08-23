77e7e4edc78fe9e40254fc795ab0474daca14259
rangpurquantum-web <rangpurquantum@gmail.com>
2026-08-23 12:15:40 +0600

    Update app-version.ts

--- lib/app-version.ts
+++ lib/app-version.ts
@@ -1,3 +1,3 @@
 // Ei number ta protibar notun APK release korle manually update korben
 export const LATEST_APK_VERSION = "1.0.0";
-export const APK_DOWNLOAD_URL = "export const APK_DOWNLOAD_URL = "https://blood-management-livid.vercel.app/app-latest.apk";
+export const APK_DOWNLOAD_URL = "https://blood-management-livid.vercel.app/app-latest.apk";k";
