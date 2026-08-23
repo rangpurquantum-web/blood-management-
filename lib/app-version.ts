7774c1445b692bea9a77246d93ed511587a2f7b2
rangpurquantum-web <rangpurquantum@gmail.com>
2026-08-23 11:20:42 +0600

    Create lib/app-version.ts

---
+++ lib/app-version.ts
@@ -0,0 +1,3 @@
+// Ei number ta protibar notun APK release korle manually update korben
+export const LATEST_APK_VERSION = "1.0.0";
+export const APK_DOWNLOAD_URL = "https://github.com/rangpurquantum-web/blood-management-/releases/latest/download/app-debug.apk";
