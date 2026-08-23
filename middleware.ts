b338eeffa004b4f01e71f9d719c2a99f5a3713fa
rangpurquantum-web <rangpurquantum@gmail.com>
2026-08-23 19:49:13 +0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,6 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|manifest.json|sw.js|icons/|offline|d/|debug/|api/debug/|^$|login|register|check-donation).*)",
+    "/((?!_next/static|_next/image|favicon.ico|manifest.json|sw.js|icons/|offline|d/|debug/|api/debug/|^$|login|register|check-donation|request-blood).*)",
   ],
 };
