753a052f3b4d96c9af8634843428d56ed5b497b9
rangpurquantum-web <rangpurquantum@gmail.com>
2026-08-25 00:40:41 +0600

    Update route.ts

--- app/api/app-version/route.ts
+++ app/api/app-version/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 
 // Notun version release korle sudhu ei duita line change korbe:
-const LATEST_VERSION_CODE = 2;
-const LATEST_VERSION_NAME = "1.1";
+const LATEST_VERSION_CODE = 3;
+const LATEST_VERSION_NAME = "1.2";
 
 const APK_DOWNLOAD_URL =
   "https://github.com/rangpurquantum-web/blood-management-/releases/download/v1.1/app-release.apk";
