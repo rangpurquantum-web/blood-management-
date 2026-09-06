b5d12d2780f06ba3095d60bcd9815c938708a8fb
rangpurquantum-web <rangpurquantum@gmail.com>
2026-09-06 16:57:58 +0600

    Fix: use donor.id instead of missing publicToken field in card route

--- app/api/donors/[id]/card/route.ts
+++ app/api/donors/[id]/card/route.ts
@@ -93,7 +93,7 @@ export const GET = withAuth(
     const baseUrl = `${protocol}://${host}`;
 
     const publicUrl =
-      `${baseUrl}/d/${donor.publicToken}`;
+      `${baseUrl}/d/${donor.id}`;
 
     // --------------------------------------------------
     // Generate QR
